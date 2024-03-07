eb63416ba863dc9dc25873c6bf47395b077b3c2e
s-chance <entropy.tree@outlook.com>
2024-03-07 20:21:10 +0800

    feat(autoconfigure module): add autoconfigure module

---
+++ dmybatis-spring-boot-autoconfigure/src/main/java/com/entropy/config/MybatisAutoConfig.java
@@ -0,0 +1,39 @@
+package com.entropy.config;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@AutoConfiguration // 自动配置类
+public class MybatisAutoConfig {
+
+    // SqlSessionFactoryBean
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean;
+    }
+
+    // MapperScannerConfigure
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(BeanFactory beanFactory) {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        // 扫描的包：启动类所在的包及其子包
+        List<String> packages = AutoConfigurationPackages.get(beanFactory);
+        String p = packages.get(0);
+        mapperScannerConfigurer.setBasePackage(p);
+
+        // 扫描的
+        mapperScannerConfigurer.setAnnotationClass(Mapper.class);
+        return mapperScannerConfigurer;
+    }
+
+}
