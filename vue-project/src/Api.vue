6a9dd36fbf617c5499fe71923c480d7d00d9bd68
s-chance <entropy.tree@outlook.com>
2024-03-11 23:49:59 +0800

    test(api.vue): test the import usage of component

---
+++ vue-project/src/Api.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+const count = ref(0);
+function increment() {
+    count.value++;
+}
+onMounted(() => {
+    console.log('mounted')
+})
+</script>
+
+<template>
+    <button @click="increment">count: {{ count }}</button>
+</template>
