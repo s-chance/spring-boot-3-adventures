e906f4652d09f6baaf492ac16a8d60a609260bb7
s-chance <entropy.tree@outlook.com>
2024-03-13 00:06:56 +0800

    feat(big-event-vue): initial big-event-vue project

---
+++ big-event-vue/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import login from '@/views/Login.vue'
+import { ref } from "vue";
+const msg = ref("Hello, Vue 3.0 + Vite");
+</script>
+
+<template>
+<!-- <h1>{{ msg }}</h1> -->
+<login/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ big-event-vue/src/utils/request.js
@@ -0,0 +1,21 @@
+// 定制请求实例
+
+// 导入 axios 
+import axios from "axios";
+// 定义公共前缀变量 baseURL
+const baseURL = 'http://localhost:8080';
+const instance = axios.create({ baseURL });
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+    result => {
+        return result.data;
+    },
+    err => {
+        alert('请求失败，请稍后重试');
+        return Promise.reject(err); // 将异步状态改为失败状态，方便被 catch 捕获
+    }
+)
+
+// 暴露请求实例
+export default instance;
