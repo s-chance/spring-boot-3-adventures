fed5506f5d62635d52762cb7d044e43bb12edb54
s-chance <entropy.tree@outlook.com>
2024-03-14 15:58:04 +0800

    perf(interceptor): add request interceptor to set token

--- big-event-vue/src/api/article.js
+++ big-event-vue/src/api/article.js
@@ -2,7 +2,8 @@ import request from '@/utils/request.js'
 import { useTokenStore } from '@/stores/token.js'
 // 文章分类列表查询
 export const articleCategoryListService = () => {
-    const tokenStore = useTokenStore()
+    // const tokenStore = useTokenStore()
     // 在 pinia 中定义的响应式数据不需要通过 .value 获取值
-    return request.get('/category', { headers: { 'Authorization': tokenStore.token } });
+    // return request.get('/category', { headers: { 'Authorization': tokenStore.token } })
+    return request.get('/category')
 }
