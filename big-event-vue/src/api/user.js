e9c17bf54ed815fc291525856c88a34a1e4c5833
s-chance <entropy.tree@outlook.com>
2024-03-15 17:21:41 +0800

    feat(update user info): support to update user info

--- big-event-vue/src/api/user.js
+++ big-event-vue/src/api/user.js
@@ -27,3 +27,8 @@ export const userLoginService = (loginData) => {
 export const userInfoService = () => {
     return request.get('/user/userInfo')
 }
+
+// 修改个人信息
+export const userInfoUpdateService = (userInfoData) => {
+    return request.put('/user/update', userInfoData)
+}
