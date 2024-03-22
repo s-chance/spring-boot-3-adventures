bb56365f1ff4f2bf62c366f01b3d1b1b092ec526
s-chance <entropy.tree@outlook.com>
2024-03-22 11:26:36 +0800

    feat(applicaioncontextinitializer): add ApplicaionContextInitializer usage demo

--- bgw/src/main/java/com/entropy/App.java
+++ bgw/src/main/java/com/entropy/App.java
@@ -1,11 +1,16 @@
 package com.entropy;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        String applicationName = context.getEnvironment().getProperty("applicationName");
+        System.out.println(applicationName);
     }
 }
---
+++ bgw/src/main/java/com/entropy/initializer/MyApplicationContextInitializer.java
@@ -0,0 +1,33 @@
+package com.entropy.initializer;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyApplicationContextInitializer implements ApplicationContextInitializer {
+
+    // ioc 容器对象创建完毕后执行
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        // 给上下文 context 对象注入环境属性
+        // 1.准备属性
+        Map<String, Object> myMap = new HashMap<>();
+        myMap.put("applicationName", "big-event");
+
+        // 2.获取一个属性资源管理对象
+        // 获取的环境对象
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        // 属性资源管理对象
+        MutablePropertySources propertySources = environment.getPropertySources();
+
+        // 3.注册
+        propertySources.addLast(new MapPropertySource("myMap", myMap));
+
+        // throw new RuntimeException();
+    }
+}
