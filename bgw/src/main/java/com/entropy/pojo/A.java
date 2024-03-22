c2d276ebdce47e683de1719502af596bf5ffa36e
s-chance <entropy.tree@outlook.com>
2024-03-22 11:42:05 +0800

    feat(pojo/ application.yaml): the demo about solve circular denpendiences problem

---
+++ bgw/src/main/java/com/entropy/pojo/A.java
@@ -0,0 +1,10 @@
+package com.entropy.pojo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class A {
+    @Autowired
+    private B b;
+}
