eeca29e89aa91bea331de9c8ef4846cef7e4d6aa
s-chance <entropy.tree@outlook.com>
2024-03-22 11:30:39 +0800

    feat(beanfactory): add BeanFactory debug demo code

--- bgw/src/main/java/com/entropy/App.java
+++ bgw/src/main/java/com/entropy/App.java
@@ -10,7 +10,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
-        String applicationName = context.getEnvironment().getProperty("applicationName");
-        System.out.println(applicationName);
+        /*String applicationName = context.getEnvironment().getProperty("applicationName");
+        System.out.println(applicationName);*/
+
+        // AnnotationConfigServletWebServerApplicationContext 最终获取到的容器类型
+        System.out.println(context.getClass());
+        App app = context.getBean(App.class);
+        System.out.println(app);
     }
 }
