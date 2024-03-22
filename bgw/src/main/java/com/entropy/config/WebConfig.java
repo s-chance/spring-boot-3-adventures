f9c5b39ec22a46278efefec68d4addbc96d5ba26
s-chance <entropy.tree@outlook.com>
2024-03-22 12:33:13 +0800

    feat(interceptor): add global interceptor

---
+++ bgw/src/main/java/com/entropy/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.entropy.config;
+
+import com.entropy.interceptor.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private MyInterceptor myInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor); // 拦截所有请求
+    }
+}
